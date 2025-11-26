69ce3a7268d7350296416db043ea8c973492e195
MohamedMiladi777 <miladyshady@gmail.com>
2025-11-26 22:12:21 +0100

    updated index.js to use memory functionality

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { runLLM } from './src/llm'
+import { addMessage, getMessages } from './src/memory'
 
 const userMessage = process.argv[2]
 
@@ -11,7 +12,12 @@ if (!userMessage) {
 //console.log(process)
 console.log(process.argv[2])
 
+//We fetch for the entire history of messages
+const messages = await getMessages()
+
 // On first run, the LLM will respond correctly but on consecutive runs it
 // will not interact with previous prompts since it has dementia..
-const response = await runLLM({ userMessage })
+
+
+const response = await runLLM({ messages: [...messages, {role: "user", content: userMessage}] })
 console.log(response)
